d1315f3246b95402cd64b0a480a95ac6c7b058bd
KolbeinnIn <kolbeinningo@nemi.tskoli.is>
2018-04-18 12:40:24 +0000

    made enemies

--- Skil03/Skil03.py
+++ Skil03/Skil03.py
@@ -2,10 +2,9 @@
 #Skilaverkefni 3 - Leikjaforritun -
 
 import pygame
-from random import *
+import random
 import os
 import klasar
-import copy
 
 os.environ["SDL_VIDEO_CENTERED"] = "300"
 pygame.init()
@@ -17,6 +16,8 @@ window = pygame.display.set_mode(window_size)
 pygame.display.set_caption('Maze')
 
 player = klasar.Player(window)
+enemy_image = pygame.image.load("images/enemy1_1.png")
+missile = pygame.image.load("images/missile.png")
 
 IMG_NAMES = ["enemy1_1" for x in range(8)]
 IMAGES = {name: pygame.image.load("images/%s.png" % str(name)).convert_alpha()
@@ -33,45 +34,67 @@ GREY = (70, 80, 90)
 YELLOW = (255, 255, 0)
 my_font = pygame.font.SysFont("", 30)
 clock = pygame.time.Clock()
-pos = []
 
-flag = False
+enemyPosition = 65
+missile_list = pygame.sprite.Group()
+enemy_list = pygame.sprite.Group()
+all_sprites_list = pygame.sprite.Group()
+all_sprites_list.add(player)
+
+
+for row in range(5):
+    for column in range(10):
+        enemy = klasar.Enemy(enemy_image)
+        enemy.rect.x = 157 + (column * 50)
+        enemy.rect.y = enemyPosition + (row * 45)
+        enemy_list.add(enemy)
+        all_sprites_list.add(enemy)
+
+
+flag = True
 teljari = 0
+teljari2 = 2
 running = True
-copied = False
+speed = 1
+listi = []
 while running:
-    pos = player.position()
     window.fill(BLACK)
     window.blit(bakgrunnur, [0, 0])
     stig = player.stig
     clock.tick(60)
+    key = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and teljari == 0:
+            shot = klasar.Missile(missile)
+            shot.rect.x = player.rect.x + 22
+            shot.rect.y = player.rect.y
+            missile_list.add(shot)
+            all_sprites_list.add(shot)
+            flag = True
 
-    key = pygame.key.get_pressed()
+    if flag and teljari < 20:
+        teljari += 1
+    if teljari >= 20:
+        teljari = 0
+        flag = False
+
     if key[pygame.K_LEFT] or key[pygame.K_a]:
         player.move(-2)
     if key[pygame.K_RIGHT] or key[pygame.K_d]:
         player.move(2)
 
-    if key[pygame.K_SPACE] and teljari == 0:
-        global skot
-        skot = klasar.Missile(pos+22)
-        flag = True
+    pygame.sprite.groupcollide(missile_list, enemy_list, True, True)
 
-    if flag and teljari < 38:    #Að nota teljara til þess að deleta skotinu er reliable vegna clock.tick(60)
-        skot.move(window)        #þá er það eins sama hversu öflug tölvan sem runnar kóðan er
-        teljari += 1
-    if teljari >= 38:
-        teljari = 0
-        flag = False
-    a = pygame.image.load("images/player.png").convert_alpha()
-    b = pygame.image.load("images/enemy1_1.png").convert_alpha()
-    window.blit(a, (player.position(), 380))
-    window.blit(b, (300, 50))
+    for shot in missile_list:
+        shot.rect.y -= 5
+    #for block in enemy_list:
+        #block.rect.x += speed
 
+    all_sprites_list.draw(window)
     pygame.display.flip()
 pygame.quit()
--- Skil03/klasar.py
+++ Skil03/klasar.py
@@ -7,13 +7,13 @@ import pygame
 clock = pygame.time.Clock()
 
 
-
-
-class Player(object):
+class Player(pygame.sprite.Sprite):
     def __init__(self, window):
-        self.image = pygame.image.load("images/player.png").convert()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/player.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = 380
+        self.rect.y = 380
         self.stig = 0
         self.window = window
 
@@ -32,125 +32,16 @@ class Player(object):
         else:
             self.rect.x = 0
 
-    def position(self):
-        return self.rect.x
 
-
-class Missile(object):
-    def __init__(self, pos):
-        self.pos = pos
-        self.image = pygame.image.load("images/missile.png").convert()  # surface
-        self.rect = self.image.get_rect(topleft=(self.pos, 380))        # rect-ið
-
-    def move(self, window):
-        self.move_single_axis(window)
-
-    def move_single_axis(self, window):
-        self.rect.y -= 10
-        window.blit(self.image, self.rect)
+class Missile(pygame.sprite.Sprite):
+    def __init__(self, missile_image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = missile_image
+        self.rect = self.image.get_rect()
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, row, column, window, IMAGES):
+    def __init__(self, enemy_image):
         pygame.sprite.Sprite.__init__(self)
-        self.IMAGES = IMAGES
-        self.window = window
-        self.row = row
-        self.column = column
-        self.images = []
-        self.load_images()
-        self.index = 0
-        self.image = self.images[self.index]
+        self.image = enemy_image
         self.rect = self.image.get_rect()
-        self.direction = 1
-        self.rightMoves = 15
-        self.leftMoves = 30
-        self.moveNumber = 0
-        self.moveTime = 600
-        self.firstTime = True
-        self.movedY = False
-        self.columns = [False] * 10
-        self.aliveColumns = [True] * 10
-        self.addRightMoves = False
-        self.addLeftMoves = False
-        self.numOfRightMoves = 0
-        self.numOfLeftMoves = 0
-        self.timer = clock.get_ticks()
-
-    def update(self, keys, currentTime, killedRow, killedColumn, killedArray):
-        self.check_column_deletion(killedRow, killedColumn, killedArray)
-        if currentTime - self.timer > self.moveTime:
-            self.movedY = False
-            if self.moveNumber >= self.rightMoves and self.direction == 1:
-                self.direction *= -1
-                self.moveNumber = 0
-                self.rect.y += 35
-                self.movedY = True
-                if self.addRightMoves:
-                    self.rightMoves += self.numOfRightMoves
-                if self.firstTime:
-                    self.rightMoves = self.leftMoves
-                    self.firstTime = False
-                self.addRightMovesAfterDrop = False
-            if self.moveNumber >= self.leftMoves and self.direction == -1:
-                self.direction *= -1
-                self.moveNumber = 0
-                self.rect.y += 35
-                self.movedY = True
-                if self.addLeftMoves:
-                    self.leftMoves += self.numOfLeftMoves
-                self.addLeftMovesAfterDrop = False
-            if self.moveNumber < self.rightMoves and self.direction == 1 and not self.movedY:
-                self.rect.x += 10
-                self.moveNumber += 1
-            if self.moveNumber < self.leftMoves and self.direction == -1 and not self.movedY:
-                self.rect.x -= 10
-                self.moveNumber += 1
-
-            self.index += 1
-            if self.index >= len(self.images):
-                self.index = 0
-            self.image = self.images[self.index]
-
-            self.timer += self.moveTime
-        self.window.blit(self.image, self.rect)
-
-    def check_column_deletion(self, killedRow, killedColumn, killedArray):
-        if killedRow != -1 and killedColumn != -1:
-            killedArray[killedRow][killedColumn] = 1
-            for column in range(10):
-                if all([killedArray[row][column] == 1 for row in range(5)]):
-                    self.columns[column] = True
-
-        for i in range(5):
-            if all([self.columns[x] for x in range(i + 1)]) and self.aliveColumns[i]:
-                self.leftMoves += 5
-                self.aliveColumns[i] = False
-                if self.direction == -1:
-                    self.rightMoves += 5
-                else:
-                    self.addRightMoves = True
-                    self.numOfRightMoves += 5
-
-        for i in range(5):
-            if all([self.columns[x] for x in range(9, 8 - i, -1)]) and self.aliveColumns[9 - i]:
-                self.aliveColumns[9 - i] = False
-                self.rightMoves += 5
-                if self.direction == 1:
-                    self.leftMoves += 5
-                else:
-                    self.addLeftMoves = True
-                    self.numOfLeftMoves += 5
-
-    def load_images(self):
-        images = {0: ["1_1", "1_1"],
-                  1: ["1_1", "1_1"],
-                  2: ["1_1", "1_1"],
-                  3: ["1_1", "1_1"],
-                  4: ["1_1", "1_1"],
-                  }
-
-        img1, img2 = (self.IMAGES["enemy{}".format(img_num)] for img_num in images[self.row])
-        self.images.append(pygame.transform.scale(img1, (40, 35)))
-        self.images.append(pygame.transform.scale(img2, (40, 35)))
-
